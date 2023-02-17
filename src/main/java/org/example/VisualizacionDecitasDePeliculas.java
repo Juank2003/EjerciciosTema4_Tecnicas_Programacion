8fbacd574bf0e8dcc943e7549aeb2ff6f2d86ef5
jkort <jortealv@myuax.com>
2023-02-17 23:00:05 +0100

    VersionPeliculas2

--- src/main/java/org/example/VisualizacionDecitasDePeliculas.java
+++ src/main/java/org/example/VisualizacionDecitasDePeliculas.java
@@ -1,5 +1,5 @@
 package org.example;
-
+/*
 import java.util.Scanner;
 
 public class VisualizacionDecitasDePeliculas {
@@ -62,4 +62,46 @@ public class VisualizacionDecitasDePeliculas {
 
     }
 
+}*/
+
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class VisualizacionDecitasDePeliculas {
+
+    public static void main(String[] args) {
+        String[] peliculas = {"Una cita de la ciudad del miedo", "Una cita de James Bond", "Una cita de la vida es un largo río tranquilo", "Una cita de Star Wars"};
+        String[][] citas = {{"Si todo el mundo es un idiota, significa que soy un idiota", "Cuidado, es una auténtica carnicería", "¿Te gusta mi máscara?", "No me subestimes, chico"}, {"Me llamo Bond, James Bond", "Agitado, no mezclado", "Mantenlo simple, Stupid", "Porque creíste que me gusta el vodka Martini, agitado no mezclado?"}, {"¿Cuál es el problema? No me he bañado en dos días, me corto las uñas de los pies una vez al mes. ¿Soy un apestoso?", "Nadie me puede obligar a ser un trabajador social", "Eso es algo que solo un padre puede entender", "Así que lo único que me queda es el suicidio, ¿verdad?"}, {"Que la fuerza te acompañe", "No trates de hacerlo, hazlo o no lo hagas, pero no lo intentes", "Luke, yo soy tu padre", "Es una trampa!"}};
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        int opcion = 0;
+
+        do {
+            System.out.println("Seleccione una opción:");
+            for (int i = 0; i < peliculas.length; i++) {
+                System.out.println((i + 1) + " - " + peliculas[i]);
+            }
+            System.out.println((peliculas.length + 1) + " - Salir de esta magnífica aplicación");
+            opcion = input.nextInt();
+
+            if (opcion >= 1 && opcion <= peliculas.length) {
+                int citaAleatoria = random.nextInt(citas[opcion - 1].length);
+                System.out.println(citas[opcion - 1][citaAleatoria]);
+            } else if (opcion == peliculas.length + 1) {
+                System.out.println("Adios");
+            } else {
+                System.out.println("Opción no válida, intente de nuevo");
+            }
+        } while (opcion != peliculas.length + 1);
+
+        input.close();
+    }
 }
+
+
+
+
+
+
